41268d9f97bcc1522f293eda512f7bf1b899e13f
Sumaxi <max@podolski.org>
2018-02-26 00:10:15 -0500

    Several Updates

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+    <div>
+        <h1>NOT FOUND</h1>
+        <p>Go back to <a href="/">home</a></p>
+    </div>
+);
+
+export default NotFoundPage;
